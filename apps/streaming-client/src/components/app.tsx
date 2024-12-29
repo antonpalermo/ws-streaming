c672ca0df047b629b94d701d77e0e139b9be42d2
antonpalermo <ANPBusinessmail@gmail.com>
2024-12-29 22:31:44 +0800

    get user media devices

--- apps/streaming-client/src/components/app.tsx
+++ apps/streaming-client/src/components/app.tsx
@@ -1,8 +1,18 @@
 export default function App() {
+  async function getDisplayMedia() {
+    const devices = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+    });
+
+    console.log(devices);
+  }
+
   return (
     <div>
       <h1>Streaming Client</h1>
       <p>Open the console to see the messages</p>
+
+      <button onClick={getDisplayMedia}>Share Screen</button>
     </div>
   );
 }
